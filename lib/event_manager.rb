a3c7b362d46b51309dd70f3cdc8fd877dc16e7e4
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-03-03 12:07:20 +1100

    Rubocoped!

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,5 @@
+# 02/03/2024 even_manager.rb
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
@@ -5,21 +7,22 @@ require 'erb'
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
 # Fetchs legislators by zipcode.
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
 # Saves file for each attendees.
 def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
@@ -29,25 +32,26 @@ def save_thank_you_letter(id, form_letter)
     file.puts form_letter
   end
 end
+
 # Cleans up the phone number.
 def clean_phone_number(phone)
   phone.gsub!(/[^0-9]/, '')
   if phone.size == 10
     phone
   else
-    phone.size == 11 && phone[0] == '1' ? phone = phone[1..11] : phone = 'xxxxxxxxxx'
+    phone = phone.size == 11 && phone[0] == '1' ? phone[1..11] : 'xxxxxxxxxx'
   end
   phone.insert(3, '-').insert(-5, '-')
 end
 
 def peak_reg_hours(reg_hour_count)
-  peak_hour = reg_hour_count.select {|index, value| value == reg_hour_count.values.max}.keys
-  return "Peak registeration hours: #{peak_hour.join(', ')} hours"
+  peak_hour = reg_hour_count.select { |_index, value| value == reg_hour_count.values.max }.keys
+  "Peak registeration hours: #{peak_hour.join(', ')} hours"
 end
 
 def peak_reg_days(reg_day_count)
-  peak_day = reg_day_count.select {|index, value| value == reg_day_count.values.max}.keys.join('')
-  return "Peak registeration day of week: #{Date::DAYNAMES[peak_day.to_i]}"
+  peak_day = reg_day_count.select { |_index, value| value == reg_day_count.values.max }.keys.join('')
+  "Peak registeration day of week: #{Date::DAYNAMES[peak_day.to_i]}"
 end
 
 puts 'EventManager initialized.'
@@ -77,8 +81,8 @@ contents.each do |row|
   reg_hour_count[reg_date_time.hour] += 1
   reg_day_count[reg_date_time.wday] += 1
 end
-puts "Attendees letters has been created."
+puts 'Attendees letters has been created.'
 puts peak_reg_hours(reg_hour_count)
 puts peak_reg_days(reg_day_count)
 
-### "event_manager finish!!!"
+### Event manager finish!!! ###
